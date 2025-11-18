0257edc4f272ee070bae11e1ac8fc37e73db4f1b
devlogix01 <shenli263031@gmail.com>
2025-11-18 20:25:16 +0800

    Update MybatisPlusDemoApplication.java

--- src/main/java/com/shenli/mybatisplusdemo/MybatisPlusDemoApplication.java
+++ src/main/java/com/shenli/mybatisplusdemo/MybatisPlusDemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.shenli.mybatisplusdemo.mapper")
 public class MybatisPlusDemoApplication {
 
     public static void main(String[] args) {
